f962ed81b5cb28cafdfec2c7926bcdfd3958b415
Maxim Gerasimenko <maximgerasimenko@bk.ru>
2024-10-14 07:40:02 +0300

    fixed build errors

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist",
+    sourcemap: true,
+  },
   resolve: {
-    alias: {
-      "@": "/src",
-    },
+    extensions: [".js", ".ts", ".jsx", ".tsx"],
   },
 });
